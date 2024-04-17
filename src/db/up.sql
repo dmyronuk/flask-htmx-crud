7e9467a467d60ca17095d4044ca8cf3a70f21dc9
D'Arcy Myronuk <darcy.myronuk@gmail.com>
2024-04-16 22:50:21 -0400

    crud calls

--- src/db/up.sql
+++ src/db/up.sql
@@ -5,8 +5,3 @@ CREATE TABLE todos (
   created_at INTEGER NOT NULL,
   updated_at INTEGER
 );
-
-INSERT INTO todos (id, content, complete, created_at, updated_at) VALUES
-  (1, 'clean kitchen', false, 0, 0),
-  (2, 'wash car', false, 0, 0),
-  (3, 'walk dog', false, 0, 0);
